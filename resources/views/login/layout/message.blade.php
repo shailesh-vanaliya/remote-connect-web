745afd0888b6d849b26cae829731ff98ac670fa4
shailesh-patel <testshailesh1@gmail.com>
2022-06-11 11:55:23 +0530

    update message

--- resources/views/login/layout/message.blade.php
+++ resources/views/login/layout/message.blade.php
@@ -1,40 +1,35 @@
-<?php /*
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-*/ ?>
-
-<div id="errorSection" style="width:100% !important;"> 
-    @if (session('session_error'))
-        <div class="alert alert-danger">
+@if (session('session_error') || session('session_success') || session('session_alert'))
+<div id="errorSection" class="col-md-12 mt-2" style="width:100% !important;">
+    <div class="col-md-12  ">
+        @if (session('session_error'))
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             {{ session('session_error') }}
-            <div class="pull-right closeIcon"><i class="fa fa-times" aria-hidden="true"></i></div>
         </div>
-    @endif
+        @endif
 
-    @if (session('session_success'))
-        <div class="alert alert-success">
+        @if (session('session_success'))
+        <div class="alert alert-success alert-dismissible">
+            <button type="button" class="close closeIcon" data-dismiss="alert" aria-hidden="true">&times;</button>
             {{ session('session_success') }}
-            <div class="pull-right closeIcon"><i class="fa fa-times" aria-hidden="true"></i></div>
         </div>
-    @endif
+        @endif
 
-    @if (session('session_alert'))
-        <div class="alert alert-warning">
+        @if (session('session_alert'))
+        <div class="alert alert-warning margin-t-30">
             {{ session('session_alert') }}
             <div class="pull-right closeIcon"><i class="fa fa-times" aria-hidden="true"></i></div>
         </div>
-    @endif
+        @endif
+    </div>
 </div>
+@endif
 <style>
-    .closeIcon{
+    .closeIcon {
         cursor: pointer;
+    }
+
+    .alert-dismissible {
+        padding-right: unset;
     }
 </style>
