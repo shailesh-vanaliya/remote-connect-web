5f519682226700a47e906f785e0d835552cc78eb
shailesh-patel <testshailesh1@gmail.com>
2022-06-29 23:12:08 +0530

    update images

--- resources/views/login/layout/header.blade.php
+++ resources/views/login/layout/header.blade.php
@@ -11,7 +11,7 @@
   ?>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Futuristic Technologies | Log in</title>
+  <title>IIOT Connect | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
